0aed642f1648b43f2759bc350dbf32b52b996c46
tstyer <travisstyer.ts@gmail.com>
2025-06-19 15:24:10 +0100

    Add first event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+
+// Event listeners // 
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You Clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You Clicked ${gametype}`)
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
